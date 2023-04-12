602da79dc535f5872adb4b3e0ccd460fd3ce2cc4
Saba <sabajaved524@gmail.com>
2023-04-12 12:12:55 +0500

    todo app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+
+const routes: Routes = [
+  {
+    path: '', component: ViewTaskComponent,
+  },
+  {
+    path: 'add-task', component: AddTaskComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from './services/task.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+  ]
 })
 export class AppComponent {
+
+
+
   title = 'AngularNewApp';
 }
